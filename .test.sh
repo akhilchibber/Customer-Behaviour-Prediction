f68290556cee26141d662b325f7c82fbd6d5e55d
Akhil Chhibber <akhil.chibber@gmail.com>
2024-09-22 21:10:50 +0200

    Update .test.sh

--- .test.sh
+++ .test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Uninstall numpy if it's already installed
+pip uninstall -y numpy
+
+# Reinstall numpy
+pip install numpy
+
 python -m mypy .
 python -m pytest -vv
